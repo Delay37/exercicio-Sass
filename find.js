cc1d2b2eaeb9449e108b1513978188881ede5265
Delay37 <benettiescritorio@gmail.com>
2025-04-27 13:08:55 -0300

    adiciona o find

--- find.js
+++ find.js
@@ -1,6 +1,32 @@
-function find() {
-    /*pega os valores e cria o array*/
-    /*pega o valor de busca*/
+import { pegavalores } from './pegavalores.js';
+
+export async function meufind() {
+    /*passa os ids a pegavalores*/
+    let idarray = "listaFind";
+    let idchave = "objetivoFind";
+
+    /*pega o array e chave*/
+    const { lista, chave } = await pegavalores(idarray, idchave);
+
     /*percorre o array buscando um valor igual*/
+    let cont = 0;
+    lista.forEach(element => {
+        if (element == chave) {
+            cont += 1;
+        }
+    });
+
     /*retorna não encontrado ou encontrado*/
+    let divresultado = document.getElementById("resultadoFind");
+    if (cont == 1 & lista[0] == "") {
+        /*bug fix clicar no botão sem ter nenhum valor não retorna 1*/
+    }
+    else if (cont >= 1  ) {
+        divresultado.style.background = " #1AAE9F";
+        divresultado.innerHTML = "<p>Encontrado!</p>";
+    }
+    else {
+        divresultado.innerHTML = "<p>Não Encontrado!</p>";
+        divresultado.style.background = " #D3455B";
+    }
 }
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import './style.scss';
 import {meufilter} from './filter.js';
+import {meufind} from './find.js';
 
 window.meufilter = meufilter;
+window.meufind = meufind;
 
 
 
